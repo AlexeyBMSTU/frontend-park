7305b49b0a074f8599fae02e8c905cc62af7e08a
AlexeyBMSTU <nap8099@gmail.com>
2024-09-15 17:44:37 +0300

    in etc

---
+++ script.js
@@ -0,0 +1,9 @@
+// const navLinks = document.querySelectorAll('nav ul li a');
+
+// navLinks.forEach((link) => {
+//   link.addEventListener('click', (event) => {
+//     event.preventDefault();
+//     const route = link.getAttribute('href');
+//     router.navigate(route);
+//   });
+// });
